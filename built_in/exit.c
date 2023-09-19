632727f32f2709751ff4df3ddfcb12d5a56bd3e5
Nora <105424123+Noralya@users.noreply.github.com>
2023-09-19 22:14:53 +0200

    fix:return value

--- built_in/exit.c
+++ built_in/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sgoigoux <sgoigoux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 14:10:12 by snaggara          #+#    #+#             */
-/*   Updated: 2023/09/14 14:28:53 by snaggara         ###   ########.fr       */
+/*   Updated: 2023/09/19 22:10:49 by sgoigoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,46 @@
     C'est la fonction appele lorsque l'utilisateur rentre la commande exit
     Attention, la commande doit tout free, ce qui n'est pas le cas la
 */
+
+int		ft_any_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if(str[i] >= '0' && str[i] <= '9')
+			return (1);
+		i++;
+	}
+	return(0);
+}
+
 void	ft_exit(t_data *data)
 {
+	t_lexer	*lexer;
+
+	lexer = data->lexer;
+	//NE PAS OUBLIER LES MESSAGES D'ERREURS
+	while (lexer && lexer->word && lexer->next != NULL)
+	{
+		lexer = lexer->next;
+		if (lexer->next == NULL)
+		{
+			if(ft_any_number(lexer->word))
+				data->exit_status = ft_atoi(lexer->word);
+			else if(lexer->index >= 4)
+				data->exit_status = 1;
+			else
+				data->exit_status = 2;
+			// /!\ Le if qui suit est à revoir 
+			if((ft_strncmp(lexer->prev->word, " ", ft_strlen(lexer->prev->word))))
+			{
+				if(lexer->prev->word[0] == '-')
+					data->exit_status += 56;
+			}
+		}
+	}
 	ft_clean_lexer(data->lexer);
 	ft_free_simple_cmd(data);
 	if (data->secret_envp)
